b830d2cb0a19256f328c5a09af19cf91bd848e8f
HOT SAUCE JAKE <get@hotsaucejake.com>
2019-02-12 06:33:29 +0000

    Apply fixes from StyleCI

--- src/Coinigy.php
+++ src/Coinigy.php
@@ -4,13 +4,12 @@ namespace hotsaucejake\Coinigy;
 
 class Coinigy
 {
-    protected $key; 
+    protected $key;
     protected $secret;
     protected $base_url;
     protected $public_url;
     protected $private_url;
     protected $curl;
-
 
     /**
      * Client constructor.
@@ -18,19 +17,22 @@ class Coinigy
      * @param array $auth
      * @param array $urls
      */
-    public function __construct(array $auth = null, array $urls = null) {
-    
-        if(!$auth) $auth = config('laravel-coinigy.auth');
-        if(!$urls) $urls = config('laravel-coinigy.urls');
+    public function __construct(array $auth = null, array $urls = null)
+    {
+        if (! $auth) {
+            $auth = config('laravel-coinigy.auth');
+        }
+        if (! $urls) {
+            $urls = config('laravel-coinigy.urls');
+        }
 
-        $this->key      = array_get($auth, 'key');
-        $this->secret   = array_get($auth, 'secret');
-        
-        $this->base_url     = array_get($urls, 'base');
-        $this->public_url   = array_get($urls, 'public');
-        $this->private_url  = array_get($urls, 'private');
+        $this->key = array_get($auth, 'key');
+        $this->secret = array_get($auth, 'secret');
+
+        $this->base_url = array_get($urls, 'base');
+        $this->public_url = array_get($urls, 'public');
+        $this->private_url = array_get($urls, 'private');
     }
-
 
     private function publicRequest($method, array $parameters = [])
     {
@@ -41,31 +43,27 @@ class Coinigy
             ],
         ];
 
-        $url = $this->public_url . $method . '?' . http_build_query(array_filter($parameters));
+        $url = $this->public_url.$method.'?'.http_build_query(array_filter($parameters));
 
         $result = json_decode(file_get_contents($url, false, stream_context_create($options)), true);
 
         return $result['success'] ? $result['result'] : $result['error'];
     }
 
-
     public function chains()
     {
         return $this->publicRequest('chains');
     }
-
 
     public function exchanges()
     {
         return $this->publicRequest('exchanges');
     }
 
-
     public function markets()
     {
         return $this->publicRequest('markets');
     }
-
 
     public function status()
     {
